a07d5548bb56bb59a4a34153d87b48bf27d53b1c
stelsemeyer <s.telsemeyer@gmail.com>
2022-12-29 18:08:06 +1100

    add app

---
+++ app/main.py
@@ -0,0 +1,22 @@
+import functions_framework
+from utils import _copy_tracks
+
+
+SOURCE_PLAYLIST_ID = os.environ.get("SOURCE_PLAYLIST_ID")
+DESTINATION_PLAYLIST_ID = os.environ.get("DESTINATION_PLAYLIST_ID")  # fmt: skip
+
+
+@functions_framework.http
+def copy_tracks(request):
+    return _copy_tracks(SOURCE_PLAYLIST_ID, DESTINATION_PLAYLIST_ID)
+
+
+# For local testing
+if __name__ == "__main__":
+    try:
+        from dotenv import load_dotenv
+
+        load_dotenv()
+    except ModuleNotFoundError:
+        pass
+    _copy_tracks(SOURCE_PLAYLIST_ID, DESTINATION_PLAYLIST_ID)
